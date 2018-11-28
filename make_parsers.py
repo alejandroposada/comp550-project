af2e90266fec8af003a21aed8044f66ea53a1e72
Joseph Viviano <vivianoj@leto38.iro.umontreal.ca>
2018-11-27 23:24:50 -0500

    new parser method now with 10% the number of entries

--- make_parsers.py
+++ make_parsers.py
@@ -2,13 +2,13 @@
 
 from nltk import induce_pcfg, treetransforms
 from nltk.corpus import ptb, treebank
-from nltk.grammar import CFG, Nonterminal
+from nltk.grammar import CFG, PCFG, Nonterminal, Production
 from nltk.parse import ShiftReduceParser
-from nltk.parse.chart import LeafInitRule, FilteredBottomUpPredictCombineRule, FilteredSingleEdgeFundamentalRule
 from nltk.parse.viterbi import ViterbiParser
+from nltk.probability import FreqDist
 import nltk
-import pickle
 import os
+import pickle
 
 def graveyard():
     #LC_STRATEGY = [
@@ -18,6 +18,26 @@ def graveyard():
     #]
     #parser = nltk.ChartParser(grammar, LC_STRATEGY, trace=2) # ??? time
     pass
+
+
+def preprocess(item):
+    return(Nonterminal(item.unicode_repr().split('-')[0].split('|')[0].split('+')[0]))
+
+
+def is_number(string):
+    try:
+        float(string)
+        return(True)
+    except:
+        return(False)
+
+
+def is_added(add_dict, item):
+    try:
+        add_dict[item.unicode_repr()]
+        return(True)
+    except:
+        return(False)
 
 
 def main():
@@ -32,16 +52,51 @@ def main():
     https://groups.google.com/forum/#!topic/nltk-users/_LXtbIekLvc
     https://www.nltk.org/_modules/nltk/grammar.html
     """
-    productions = []
+    freq_thresh = 30
+    word_freqs = FreqDist(ptb.words())
 
     if not os.path.isfile('grammar.pkl'):
 
+        productions = []
+        add_dict = {}
+
         # use the entire treebank's parsed sentences to generate the CFG
-        for tree in ptb.parsed_sents():
+        for i, tree in enumerate(ptb.parsed_sents()):
+
+            # is it a good idea to combine this with my preprocessing?
             tree.collapse_unary(collapsePOS=False)
-            tree.chomsky_normal_form(horzMarkov=1)
-            productions += tree.productions()
-        productions = list(set(productions))
+            tree.chomsky_normal_form(horzMarkov=2)
+
+            # preprocess all productions by removing all tags
+            these_productions = tree.productions()
+            for production in these_productions:
+
+                # remove all tags from the LHS (only keep primary tag)
+                production._lhs = preprocess(production._lhs)
+
+                rhs = []
+                for item in production._rhs:
+
+                    # remove all tags from the Nonterminals on the RHS
+                    if type(item) == nltk.grammar.Nonterminal:
+                        rhs.append(preprocess(item))
+
+                    # replace numbers with #
+                    elif is_number(item):
+                        rhs.append('#')
+
+                    # replace infrequent words with <unk>
+                    elif word_freqs[item] < freq_thresh:
+                        rhs.append('<unk>')
+
+                    else:
+                        rhs.append(item)
+
+                production._rhs = tuple(rhs)
+
+                if not is_added(add_dict, production):
+                    add_dict[production.unicode_repr()] = True
+                    productions.append(production)
 
         grammar = induce_pcfg(Nonterminal('S'), productions)
 
