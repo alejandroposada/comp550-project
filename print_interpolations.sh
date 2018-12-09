adcb64c1b0c83b4987bd3bc18590fbce8bed1a7c
Joseph Viviano <vivianoj@leto26.iro.umontreal.ca>
2018-12-09 12:15:59 -0500

    updated trained models

--- print_interpolations.sh
+++ print_interpolations.sh
@@ -2,7 +2,7 @@
 
 ./inference.py \
     --load_vae model/E99.pytorch \
-    --load_actor save_model/actor_model49.path.tar \
-    -n 5
-#    --constraint_mode \
+    --load_actor save_model/actor_model99.path.tar \
+    -n 5 \
+    --constraint_mode
 
