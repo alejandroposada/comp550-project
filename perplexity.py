fd15d8ac75c00742f214c770a3bde76fd1d58697
Alejandro Posada <a.posada10@uniandes.edu.co>
2018-12-12 22:29:43 -0500

    perplexity stuff

--- perplexity.py
+++ perplexity.py
@@ -1,30 +1,56 @@
 #!/usr/bin/env python
 
-from utils import load_pickle
 from collections import OrderedDict
 from ptb import PTB
 import argparse
 import numpy as np
-from nltk.probability import FreqDist
+import collections
 
 
-#def remove_pad(tokens):
-
-def perplexity(testset, freqs):
+# computes perplexity of the unigram model on a test set
+def perplexity(testset, model):
     perplexity = 0
-    N = float(freqs.N())
+    N = sum([len(sentence.split()) for sentence in testset])
 
     for sentence in testset:
-        perplexity += np.log2(sentence_prob(sentence, freqs))
+        perplexity += np.log2(sentence_prob(sentence, model))
+    return 2 ** (-1 / N * perplexity)
 
-    return(2**( (-1/N) * perplexity) )
 
-
-def sentence_prob(sentence, freqs):
+def sentence_prob(sentence, model):
     prob = 1
-    for word in sentence:
-        prob *= freqs.freq(word)
-    return(prob)
+    for word in sentence.split():
+        prob *= model[word]
+    return prob
+
+
+# here you construct the unigram language model
+def unigram(tokens):
+    stops = ['<', '>']
+    model = collections.defaultdict(lambda: 0.001)
+    for f in tokens:
+        if f in stops:
+            continue
+        try:
+            model[f] += 1
+        except KeyError:
+            model[f] = 1
+            continue
+    total = sum(model.values())
+    for word in model:
+        model[word] = model[word] / total
+
+    model['<unk>'] = model['unk']
+    del model['unk']
+    return model
+
+
+def load_prime_samples(path):
+    d = np.load(path)
+    samples = []
+    for k, v in d.items():
+        samples += v
+    return samples
 
 
 def make_corpus(dataset):
@@ -53,13 +79,20 @@ def main(args):
         min_occ=args.min_occ
     )
 
-
     corpus = make_corpus(datasets['valid'])
-    freqs = FreqDist(corpus)
-    sents_sample = load_pickle('samples/sents_sample_n250.pkl')
-    sents_sample_prime = load_pickle('samples/sents_sample_prime_n250.pkl')
+    model = unigram(corpus)
 
-    import IPython; IPython.embed()
+    # Load samples
+    samples_prime = load_prime_samples('samples/sents_sample_prime_n250.pkl')
+    samples = np.load('samples/sents_sample_n250.pkl')
+    samples_prime_dict = np.load('samples/sents_sample_prime_n250.pkl')
+
+    # Compute and display perplexities
+    print('Perplexity with constraints: {:.2f}'.format(perplexity(samples_prime, model)))
+    print('Per tag:')
+    for k in samples_prime_dict.keys():
+        print('\t' + k + ': ', perplexity(samples_prime_dict[k], model))
+    print('Perplexity without constraints: {:.2f}'.format(perplexity(samples, model)))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
